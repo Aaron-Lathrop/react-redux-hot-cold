a6d9d04db316848615b2f9122c9f3cb675dd600d
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-12 15:35:28 -0700

    connected remaining components to redux

--- src/components/game.js
+++ src/components/game.js
@@ -7,50 +7,15 @@ import InfoSection from './info-section';
 
 
 
-export default class Game extends React.Component {
-  makeGuess(guess) {
-    this.props.dispatch(makeGuess(guess));
-  }
-
-  generateAuralUpdate(auralStatus) {
-    this.props.dispatch(generateAurualUpdate(auralStatus));
-  }
-
-  restartGame() {
-    this.props.dispatch(restartGame());
-  }
-
-  render() {
-    const { feedback, guesses, auralStatus } = this.props;
-    const guessCount = guesses.length;
-
-    return (
-      <div>
-        <Header
-          onRestartGame={() => this.restartGame()}
-          onGenerateAuralUpdate={() => this.generateAuralUpdate()}
-        />
-        <main role="main">
-          <GuessSection
-            feedback={feedback}
-            guessCount={guessCount}
-            onMakeGuess={guess => this.makeGuess(guess)}
-          />
-          <StatusSection guesses={guesses} 
-            auralStatus={auralStatus}
-          />
-          <InfoSection />
-        </main>
-      </div>
-    );
-  }
+export default function Game(props) {
+  return (
+    <div>
+      <Header />
+      <main role="main">
+        <GuessSection />
+        <StatusSection />
+        <InfoSection />
+      </main>
+    </div>
+  );
 }
-
-const mapStateToProps = state => ({
-  guesses: state.guesses,
-  feedback: state.feedback,
-  auralStatus: state.auralStatus,
-  correctAnswer: state.correctAnswer
-});
-
-export default connect(mapStateToProps)(Game);
